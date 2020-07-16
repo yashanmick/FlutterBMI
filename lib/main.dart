f793de1000c1a6178548853f94c0d8825eb9d23c
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-07-16 23:25:03 +0530

    Theming

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,17 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF4C5065),
+        //this changes the color of main components such as app bar, nav bar (but not background)
+        accentColor: Color(0xFF8A92AD),
+        textTheme: TextTheme(
+          body1: TextStyle(
+              color:
+                  Color(0xFFFFFFFF)), //dark theme already have white color text
+        ),
+      ),
       home: InputPage(),
     );
   }
@@ -26,9 +37,15 @@ class _InputPageState extends State<InputPage> {
       body: Center(
         child: Text('Body Text'),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
+      // floatingActionButton: Theme(
+      //   //customizing the button by adding theme properties
+      //   data: ThemeData(
+      //     accentColor: Color(0xFF8A92AD),
+      //   ),
+      //   child: FloatingActionButton(
+      //     child: Icon(Icons.add),
+      //   ),
+      // ),
     );
   }
 }
