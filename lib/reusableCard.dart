6e7224e9988c72ffa48dcab802a6624ff0658ab8
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-07-17 13:21:23 +0530

    add GestureDetector inside the ReusableCard. (optimized the functionality)

--- lib/reusableCard.dart
+++ lib/reusableCard.dart
@@ -8,19 +8,23 @@ class ReusableCard extends StatelessWidget {
   //   Key key,
   // }) : super(key: key);
 
-  ReusableCard({@required this.colour, this.cardChild});
+  ReusableCard({@required this.colour, this.cardChild, this.onPress});
   //final keyword makes colour variable imutable
   final Color colour;
   final Widget cardChild;
+  final Function onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: colour,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: colour,
+        ),
       ),
     );
   }
